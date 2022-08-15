0f5f62651e5e3c22298da8b96b7f2919ecf8f8f4
henrygit2001 <92187605+henrygit2001@users.noreply.github.com>
2022-08-15 02:32:42 -0700

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const flightScanner = require('skiplagged-node-api');
 const app = express();
-var cors = require('cors');
-app.use(cors());
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
