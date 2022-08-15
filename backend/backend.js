28ebc5814c0dd01eafb2dc9d052b5767eed86a49
henrygit2001 <92187605+henrygit2001@users.noreply.github.com>
2022-08-15 02:13:41 -0700

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -57,59 +57,6 @@ app.post('/data', async function (req, res) {
   await Flight_Scan();
 });
 app.get('/', function (req, res) {
-  console.log(resp)
-  res.send([
-  [
-    [ 'Spirit Airlines', 'Spirit Airlines' ],
-    [ 'Spirit Airlines', 'Spirit Airlines' ],
-    [ 'Spirit Airlines', 'Spirit Airlines' ],
-    [ 'Spirit Airlines', 'Spirit Airlines' ],
-    [ 'Alaska Airlines', 'Alaska Airlines' ],
-    [ 'Alaska Airlines', 'Alaska Airlines' ],
-    [ 'Alaska Airlines', 'Alaska Airlines' ],
-    [ 'Alaska Airlines', 'Alaska Airlines' ],
-    [ 'Alaska Airlines', 'Alaska Airlines' ],
-    [ 'Alaska Airlines', 'Alaska Airlines' ],
-    [ 'Alaska Airlines', 'Alaska Airlines' ],
-    [ 'Alaska Airlines', 'Alaska Airlines' ]
-  ],
-  [
-    '$152.00', '$152.00',
-    '$152.00', '$152.00',
-    '$208.00', '$208.00',
-    '$208.00', '$208.00',
-    '$208.00', '$208.00',
-    '$208.00', '$208.00'
-  ],
-  [
-    '4 Hours 40 Minutes',
-    '7 Hours 1 Minute',
-    '9 Hours 22 Minutes',
-    '11 Hours 20 Minutes',
-    '9 Hours 35 Minutes',
-    '11 Hours 50 Minutes',
-    '14 Hours 25 Minutes',
-    '13 Hours 35 Minutes',
-    '14 Hours 12 Minutes',
-    '16 Hours 27 Minutes',
-    '16 Hours 50 Minutes',
-    '18 Hours 12 Minutes'
-  ],
-  [
-    'Friday, August 26th 2022, 06:45pm=>Friday, August 26th 2022, 11:25pm',
-    'Friday, August 26th 2022, 04:24pm=>Friday, August 26th 2022, 11:25pm',
-    'Friday, August 26th 2022, 02:03pm=>Friday, August 26th 2022, 11:25pm',
-    'Friday, August 26th 2022, 12:05pm=>Friday, August 26th 2022, 11:25pm',
-    'Friday, August 26th 2022, 11:00pm=>Saturday, August 27th 2022, 08:35am',
-    'Friday, August 26th 2022, 08:45pm=>Saturday, August 27th 2022, 08:35am',
-    'Friday, August 26th 2022, 06:10pm=>Saturday, August 27th 2022, 08:35am',
-    'Friday, August 26th 2022, 07:00pm=>Saturday, August 27th 2022, 08:35am',
-    'Friday, August 26th 2022, 11:00pm=>Saturday, August 27th 2022, 01:12pm',
-    'Friday, August 26th 2022, 08:45pm=>Saturday, August 27th 2022, 01:12pm',
-    'Friday, August 26th 2022, 11:00pm=>Saturday, August 27th 2022, 03:50pm',
-    'Friday, August 26th 2022, 07:00pm=>Saturday, August 27th 2022, 01:12pm'
-  ],
-  12
-]);
-});
+  console.log(resp);
+  res.send(resp);
 app.listen(port, () => console.log('listening on: ' + port));
