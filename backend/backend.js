495ee10ddb0192d150d75b40d5630190d1d92e29
henrygit2001 <92187605+henrygit2001@users.noreply.github.com>
2022-08-16 12:03:35 -0700

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -51,7 +51,7 @@ async function Flight_Scan() {
 app.post('/', async function(req,res){
   await Flight_Scan();
   res.send(resp);
-}
+});
 app.post('/data', async function (req, res) {
   searchOptions.resultsCount = Number(req.body.Results_Count);
   searchOptions.from= String(req.body.From);
